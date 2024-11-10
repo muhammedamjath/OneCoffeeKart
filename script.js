c005d82e828d4481db407f332aeb8cfe4e3d7ee0
Amjath <muhammedamjath0@gmail.com>
2024-11-11 01:07:42 +0530

    design updated

--- script.js
+++ script.js
@@ -10,5 +10,36 @@ document.addEventListener("DOMContentLoaded", () => {
         closeIcon.classList.toggle('hidden');
     });
 
-    console.log('hello');
+    $(document).ready(() => {
+        $("#formSubmit").submit((e) => {
+            e.preventDefault();
+            $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbzGu1rQX-cAim2_Ef3LPPBMWpg2gQ52cVxZfxldJ2DluNmBDujUe9w8mOhcl5Hdq8Vk/exec",
+                data: $("#formSubmit").serialize(),
+                method: "post",
+                success: function (response) {
+                    alert("Form submitted successfully");
+                    window.location.reload();
+                },
+                error: function (err) {
+                    alert("Something went wrong");
+                },
+            });
+        });
+    });
 });
+
+
+window.onload = function() {
+    const lat = 11.60588599403685;  
+    const lng =  76.0036856796964
+
+    const map = L.map('map').setView([lat, lng], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    const marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup("<b>One Coffee Kart.</b><br>Achooranam , 6th mile").openPopup();
+}
